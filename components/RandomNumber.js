b577d0388964e474298b3e718bd252a38c01ea5a
ssangdon <tngustkdehs@naver.com>
2021-12-22 19:49:25 +0900

    1234

--- components/RandomNumber.js
+++ components/RandomNumber.js
@@ -149,7 +149,7 @@ const RandomNumber = () => {
             container
             item
             justifyContent="center"
-            xs={7}
+            xs={8}
             alignItems="center"
           >
              {numbers.map((d) => (
